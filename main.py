c4e7ce309035ab50c304ad116af5b7ad3a14cd98
JoshuaSitumeang <160564706+JoshuaSitumeang@users.noreply.github.com>
2024-03-28 07:23:57 +0000

    Assignment 6 Trial

--- main.py
+++ main.py
@@ -4,17 +4,49 @@ import time
 
 # Part 1
 def clock(n):
+    """
+    Shows the time and update it once every second
+    """
     # Your code here
-    pass
+while n > 0:
+
+    print(strftime: (HH:MM:SS))
+    
 
 
 # Part 2
 def lcm(a, b):
+    """
+    Identify the lowest common multiple for 2 numbers
+    """
     # Your code here
-    pass
+a = input('Enter an integer value (integer): ')
+b = input('Enter an integer value (integer):')
+if a == b:
+    print (a == b == lcm)
+else:
+    if a > b: 
+        print(int("a += a_original"))
+    else:
+        print(int("b += b_original"))
+ while a > b:
+     print (int("lcm"))
+    
 
 
 # Part 3
 def gcf(a, b):
+    """
+    Identify the greatest common factor of 2 integers
+    """
     # Your code here
-    pass
+a = input('Enter an integer value (integer): ')
+b = input('Enter an integer value (integer):')
+    if a == b:
+        print (a == b == gcf)
+    else:
+if b == 0:
+    return (int(a))
+else:
+    r = divmod(a, b)
+
